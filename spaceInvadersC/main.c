ed77f72ecf7a777d885ff27c13f3e8784d2a4dec
fontan10 <josephfontanilla2@gmail.com>
2020-04-03 07:57:39 -0400

    Wrote function which draws black over a rectangle.

--- spaceInvadersC/main.c
+++ spaceInvadersC/main.c
@@ -5,14 +5,18 @@
 void swap(int* first, int* second);
 void clearScreen();
 void plotPixel(int x, int y, short int lineColor);
-void drawLine(int x0, int y0, int x1, int y1, short int color);
 void waitForVSync();
-void drawBoxesAndLines(short colorBox[], int dxBox[], int dyBox[], int xBox[], int yBox[]);
-void drawBox(int x, int y, short color);
-void updateBoxLocations(int dxBox[], int dyBox[], int xBox[], int yBox[]);
 
-int SCREEN_WIDTH = 320; //X
-int SCREEN_HEIGHT = 240; //Y
+//this function draws black from xInit to (xInit + width)
+//and yInit to (yInit + height)
+void drawBlack(int xInit, int yInit, int width, int height);
+
+void drawPlayer(int xInit, int yInit);
+const int PLAYER_WIDTH = 11;
+const int PLAYER_HEIGHT = 8;
+
+const int SCREEN_WIDTH = 320; //X
+const int SCREEN_HEIGHT = 240; //Y
 
 volatile int pixelBufferStart; // global variable
 const int n = 8;
@@ -30,21 +34,6 @@ const short int playerIcon[8][11] = {
 
 int main(void) {
     volatile int* pixelCtrlPtr = (int*)0xFF203020;
-    short colors[10] = { 0xFFFF, 0xF800, 0x07E0, 0x001F, 0xF1FF, 0xF0E0, 0x9E60, 0x07F7, 0x765F, 0x1058 };
-    short colorBox[n];
-    int dxBox[n];
-    int dyBox[n];
-    int xBox[n];
-    int yBox[n];
-
-    // initialize location and direction of rectangles
-    for (int i = 0; i < n; ++i) {
-        colorBox[i] = colors[rand() % 10];
-        dxBox[i] = ((rand() % 2) * 2) - 1;
-        dyBox[i] = ((rand() % 2) * 2) - 1;
-        xBox[i] = rand() % SCREEN_WIDTH;
-        yBox[i] = rand() % SCREEN_HEIGHT;
-    }
 
     /* set front pixel buffer to start of FPGA On-chip memory */
     *(pixelCtrlPtr + 1) = 0xC8000000; // first store the address in the 
@@ -59,102 +48,34 @@ int main(void) {
     *(pixelCtrlPtr + 1) = 0xC0000000;
     pixelBufferStart = *(pixelCtrlPtr + 1); // we draw on the back buffer
 
+    clearScreen();
 
     // continually draw the screen
-    clearScreen();
     while (1) {
+        drawPlayer(80, 50);
 
-        int count = 0;
-        for (int y = 0; y < 8; ++y) {
-            for (int x = 0; x < 11; ++x) {
-                plotPixel(x, y, playerIcon[y][x]);
-                count++;
-            }
-        }
 
         waitForVSync(); // swap front and back buffers on VGA vertical sync
         pixelBufferStart = *(pixelCtrlPtr + 1); // new back buffer
     }
 }
 
-void drawBoxesAndLines(short colorBox[], int dxBox[], int dyBox[], int xBox[], int yBox[]) {
-    for (int i = 0; i < n; ++i) {
-        drawBox(xBox[i], yBox[i], colorBox[i]);
-        drawLine(xBox[i], yBox[i], xBox[(i + 1) % n], yBox[(i + 1) % n], colorBox[i]);
-    }
-}
-
-void drawBox(int x, int y, short color) {
-    if ((x + 2 < SCREEN_WIDTH) && (x > 0) && (y + 2 < SCREEN_HEIGHT) && (y > 0)) {
-        plotPixel(x, y, color);
-        plotPixel(x, y + 1, color);
-        plotPixel(x, y + 2, color);
-
-        plotPixel(x + 1, y, color);
-        plotPixel(x + 1, y + 1, color);
-        plotPixel(x + 1, y + 2, color);
-
-        plotPixel(x + 2, y, color);
-        plotPixel(x + 2, y + 1, color);
-        plotPixel(x + 2, y + 2, color);
-    }
-}
-
-void updateBoxLocations(int dxBox[], int dyBox[], int xBox[], int yBox[]) {
-    for (int i = 0; i < n; ++i) {
-        xBox[i] += dxBox[i];
-        yBox[i] += dyBox[i];
-
-        if (xBox[i] + 3 >= SCREEN_WIDTH) {
-            dxBox[i] = -1;
-        }
-        else if (xBox[i] <= 0) {
-            dxBox[i] = 1;
-        }
-
-        if (yBox[i] + 3 >= SCREEN_HEIGHT) {
-            dyBox[i] = -1;
-        }
-        else if (yBox[i] <= 0) {
-            dyBox[i] = 1;
+void drawPlayer(int xInit, int yInit) {
+    for (int y = 0; y < PLAYER_HEIGHT; ++y) {
+        for (int x = 0; x < PLAYER_WIDTH; ++x) {
+            plotPixel(xInit + x, yInit + y, playerIcon[y][x]);
         }
     }
 }
 
-
-void drawLine(int x0, int y0, int x1, int y1, short int color) {
-    bool isSteep = abs(y1 - y0) > abs(x1 - x0);
-
-    if (isSteep) {
-        swap(&x0, &y0);
-        swap(&x1, &y1);
-    }
-
-    if (x0 > x1) {
-        swap(&x0, &x1);
-        swap(&y0, &y1);
-    }
-
-    int deltaX = x1 - x0;
-    int deltaY = abs(y1 - y0);
-    int error = -(deltaX / 2);
-    int y = y0;
-    int yStep;
-
-    if (y0 < y1) { yStep = 1; }
-    else { yStep = -1; }
-
-    for (int x = x0; x <= x1; ++x) {
-        if (isSteep) { plotPixel(y, x, color); }
-        else { plotPixel(x, y, color); }
-
-        error += deltaY;
-        if (error >= 0) {
-            y += yStep;
-            error -= deltaX;
+void drawBlack(int xInit, int yInit, int width, int height) {
+    for (int x = xInit; x < xInit + width && x < SCREEN_WIDTH; ++x) {
+        for (int y = yInit; y < yInit + height && y < SCREEN_HEIGHT; ++y) {
+            plotPixel(x, y, 0x000000);
         }
     }
 }
+
 
 void plotPixel(int x, int y, short int lineColor) {
     *(short int*)(pixelBufferStart + (y << 10) + (x << 1)) = lineColor;
